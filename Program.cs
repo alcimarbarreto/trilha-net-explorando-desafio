4929b93d8fe4c4efb67a41f10dab63f5be8bef03
Alcimar Barreto <alcimargomes@gmail.com>
2025-07-01 01:28:22 -0300

    Resolução do Desafio hospedagem de Hotel

--- Program.cs
+++ Program.cs
@@ -4,22 +4,51 @@ using DesafioProjetoHospedagem.Models;
 Console.OutputEncoding = Encoding.UTF8;
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
+
+Console.Write("Digite o número de hóspedes: ");
+int quantidadeHospedes = int.Parse(Console.ReadLine());
+
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
-
-hospedes.Add(p1);
-hospedes.Add(p2);
+// Loop para adicionar cada hóspede com entrada de nome
+for (int i = 1; i <= quantidadeHospedes; i++)
+{
+    Console.Write($"Digite o nome do hóspede {i}: ");
+    string nomeHospede = Console.ReadLine();
+    hospedes.Add(new Pessoa(nome: nomeHospede));
+}
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Console.Write("Digite a capacidade da suíte: ");
+int capacidadeSuite = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor da diária da suíte: ");
+decimal valorDiaria = decimal.Parse(Console.ReadLine());
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: capacidadeSuite, valorDiaria: valorDiaria);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Console.Write("Digite a quantidade de dias reservados: ");
+int diasReservados = int.Parse(Console.ReadLine());
+
+if (diasReservados <= 0)
+{
+    throw new ArgumentException("O número de dias reservados deve ser maior que zero.");
+}
+
+Reserva reserva = new Reserva(diasReservados: diasReservados);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine("\n");
+Console.WriteLine($"Suíte escolhida: {suite.TipoSuite}");
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+Console.WriteLine("Lista de hóspedes:");
+foreach (var hospede in hospedes)
+{
+    Console.WriteLine($" -  {hospede.Nome}");
+}
+
+Console.WriteLine($"Total a pagar por {reserva.DiasReservados} dias: {reserva.CalcularValorDiaria():C}");
